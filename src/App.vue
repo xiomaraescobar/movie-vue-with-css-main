b9ff2d52b68bfc50a5ef8c663c95efdbe4251825
Your Name <you@example.com>
2024-08-29 14:05:08 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Form from "./components/Form.vue";
+</script>
+
+<template>
+  <h2>Peliculas</h2>
+  <Form />
+</template>
+
+<style lang="scss">
+@import "./styles/variables";
+
+html,
+body {
+  background: $bg;
+  color: $color;
+  margin: 0;
+  font-family: sans-serif;
+}
+
+h2 {
+  border-bottom:
+    1px solid $color;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const movies = ref([
+
+  {
+    name: "Baturu Man",
+    available: 8,
+    quantity: 0,
+  },
+  {
+    name: "Journey Rabbit",
+    available: 2,
+    quantity: 0,
+  },
+  {
+    name: "panter pink",
+    available: 3,
+    quantity: 0,
+  },
+  {
+    name: "River Gens, Mid Game",
+    available: 5,
+    quantity: 0,
+  },
+]);
+</script>
+
+<template>
+  <form v-for="(movie, i) in movies" :key="i">
+    <h4>{{ movie.name }}</h4>
+    <button type="button" @click="movie.quantity--" :disabled="movie.quantity <= 0">
+    -
+    </button>
+    {{ movie.quantity }}
+    <button type="button" @click="movie.quantity++" :disabled="(movie.quantity >= movie.available)">
+    +
+    </button>
+  </form>
+</template>
+
+
+<style lang="scss" scoped>
+@import "../styles/_variables";
+
+form {
+  background: $bg;
+  border: 1px solid $color;
+  margin: 0 50px 25px;
+  font-family: courier;
+  padding: 10px 25px 25px;
+  text-align: center;
+  transform: scale(1);
+  transition: 0.3s transform;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+}
+
+button {
+  background: $color2;
+  border: 1px solid $color;
+  cursor: pointer;
+  padding: 5px 10px;
+
+  &:hover {
+    border-color: $color2;
+  }
+}
+
+button[disabled] {
+  opacity: 0.5;
+}
+</style>
